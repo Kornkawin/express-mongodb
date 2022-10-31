7a4fd17bf5501fec81505b74de2c588e7c1b7fc4
Kornkawin Bijayayothin <outsource1456@setth.onmicrosoft.com>
2022-10-31 16:58:38 +0700

    add many response

--- app.js
+++ app.js
@@ -1,20 +1,53 @@
+const fs = require('fs');
 const express = require('express'); // CommonJS (CJS) for Node for backend
 // import app from 'app'; // ES Module (ESM) for frontend
 
 const port = 3000;
 const app = express();
 
+
 app.get('/', (req, res) => {
-  res.send('HOME');
+  res.format({
+    'text/html': () => {
+      res.send('<h1>Hello world!</h1>');
+    },
+    'text/plain': () => {
+      res.set('Content-Type', 'text/plain'); // set Header (Content-Type)
+      res.send('Hello world!');
+    },
+    'application/json': () => {
+      res.send({
+        text: 'Hello world!'
+      });
+    },
+    default: () => {
+      res.status(400).send('Content-Type not allowed');
+    }
+  })
 });
 
-app.get('/ping', (req, res) => {
-  res.send('pong');
+app.get('/cat', async (req, res) => {
+  const buff = await fs.promises.readFile('./img/cat.jpeg');
+  res.send(buff); // send img buffer via http response
 });
 
-app.get('/abc', (req, res) => {
-  res.send('def');
-})
+app.get('/image', (req, res) => {
+  // res.download('./img/cat.jpeg'); // more appropriate for file download
+  res.download('./img/cat.jpeg', 'cat.jpg'); // able to change image name
+});
+
+app.get('/google', (req, res) => {
+  res.redirect(301, 'https://www.google.com');
+});
+
+app.get('/notfound', (req, res) => {
+  res.status(404).send('not found!');
+});
+
+app.get('/notallow', (req, res) => {
+  res.status(403).send('not allowed!');
+});
+
 
 app.listen(port, () => {
   console.log(`listening on port ${port}`);
